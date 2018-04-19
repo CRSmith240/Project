116af0d8572de7958b20b8e7046afc4a24d94c08
Cody <crsmith240@gmail.com>
2018-04-18 23:52:43 -0400

    commit test2

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace project {
     public class Program {
         public static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi");
         }
     }
 }
